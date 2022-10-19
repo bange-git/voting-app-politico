80864417a30b36ee7175f522f2848d5573bf18df
bange-git <sebastienbange@gmail.com>
2022-10-19 09:23:26 +0100

    fixed import directory for github

--- static/assets/index.ts
+++ static/assets/index.ts
@@ -1,6 +1,6 @@
 import FacebookLogoSvg from "./svg/Facebook_logo.svg";
 import GoogleLogoSvg from "./svg/Google_logo.svg";
-import GitHubLogoSvg from "./svg/GitHub_logo.svg";
+import GitHubLogoSvg from "./svg/Github_logo.svg";
 
 export const FacebookLogo = FacebookLogoSvg;
 export const GoogleLogo = GoogleLogoSvg;
